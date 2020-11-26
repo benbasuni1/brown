3d4a155eceb56edf30204c746d77625a7dfdb684
Ben Basuni <benbasuni1@gmail.com>
2020-11-25 22:19:27 -0800

    valid sudoku

--- 3_b2bswe/__pseudo/02_arrays/4e_validSudoku.js
+++ 3_b2bswe/__pseudo/02_arrays/4e_validSudoku.js
@@ -10,10 +10,11 @@
 		rowMap{}, colMap{}, boxMap{}
 		(j < 9):
 			row = m.i.j; col = m.j.i
-			box = m[3(i/3) + 3(j/3)][3(i%3) + 3(j%3)]
+			box = m[3(i/3) + 3(j/3)]
+				   [3(i%3) + 3(j%3)]
 
 			if (row|col|box === '.')
-				if (row|col|box| in map) return false
+				if (map[row|col|box]) return false
 				map[row|col|box] = 1
 
 	return true
