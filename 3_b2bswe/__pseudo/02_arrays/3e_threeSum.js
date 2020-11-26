b8bbeedf2956499610f2406d19cf6552e0c87468
Ben Basuni <benbasuni1@gmail.com>
2020-11-25 22:34:25 -0800

    3sumg

--- 3_b2bswe/__pseudo/02_arrays/3e_threeSum.js
+++ 3_b2bswe/__pseudo/02_arrays/3e_threeSum.js
@@ -10,15 +10,15 @@
 	result = []
 
 	(i < len(a) - 2):
-		if (a.i === a.i - 1) continue
+		if (a.i == a.i - 1) continue
 		left, right;
 		while (left < right):
 			sum = a.i + a.left + a.right
-			if (sum === 0):
-				result.push(a.i, a.left, a.right);
+			if (sum == 0):
+				+(a.i, a.left, a.right);
 				left++; right--; 
-				while(a.left === a.left - 1): left++
-				while(a.right === a.right + 1): right--
+				while(a.left == a.left - 1): left++
+				while(a.right == a.right + 1): right--
 			elif (sum < 0) left++;
 			else right--;
 
