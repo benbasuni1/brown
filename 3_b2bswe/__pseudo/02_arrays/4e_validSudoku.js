45409c877fdc99163c9885623dd55f988ac34c0c
Ben Basuni <benbasuni1@gmail.com>
2020-11-25 22:23:00 -0800

    as

--- 3_b2bswe/__pseudo/02_arrays/4e_validSudoku.js
+++ 3_b2bswe/__pseudo/02_arrays/4e_validSudoku.js
@@ -11,9 +11,9 @@
 		(j < 9):
 			row = m.i.j; col = m.j.i
 			box = m[3(i/3) + 3(j/3)]
-				    [3(i%3) + 3(j%3)]
+				   [3(i%3) + 3(j%3)]
 
-			if (row|col|box !== '.')
+			if (row|col|box != '.')
 				if (map[row|col|box]) return false
 				map[row|col|box] = 1
 
