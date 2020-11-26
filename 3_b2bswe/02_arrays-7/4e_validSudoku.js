7dd333911e6123bae63c05fa6eec0b02862b9ba7
Ben Basuni <benbasuni1@gmail.com>
2020-11-25 22:22:12 -0800

    fixed valid sudoku

--- 3_b2bswe/02_arrays-7/4e_validSudoku.js
+++ 3_b2bswe/02_arrays-7/4e_validSudoku.js
@@ -1,17 +1,51 @@
-// 1. Rotate 90 Degrees
-size = len(a) - 1;
+const { helper: utils, log, line } = require('../../__common/utils');
+ 
+// 🕑 O(mn)
+// 🛰 O(n)
+const validSudoku = m => {
+	utils.start(m);
+	for (let i = 0; i < 9; i++) {
+		let rowMap = {};
+		let colMap = {};
+		let boxMap = {};
 
-(layer = 0; layer < len(a) / 2) 
-	(i = layer; i < size - layer) 
-		top   = a[layer]        [i]
-		right = a[i]            [size - layer]
-		bot   = a[size - layer] [size - i]
-		left  = a[size - i]     [layer]
+		for (let j = 0; j < 9; j++) {
+			let row = m[i][j];
+			let col = m[j][i];
+			let box = m[3 * Math.floor(i / 3) + Math.floor(j / 3)][3 * (i % 3) + (j % 3)];
+			
+			if (row !== '.') {
+				if (row in rowMap) return false;
+				rowMap[row] = 1;
+			}
+			
+			if (col !== '.') {
+				if (col in colMap) return false;
+				colMap[col] = 1;
+			}
 
-		a[layer]        [i]            = left
-		a[i]            [size - layer] = top
-		a[size - layer] [size - i]     = right
-		a[size - i]     [layer]        = bot
+			if (box !== '.') {
+				if (box in boxMap) return false;
+				boxMap[box] = 1;
+			}
+		}
+	}
 
-return a;
-
+	return true;
+}
+ 
+const main = () => {
+	log(validSudoku([
+		["5","3",".",".","7",".",".",".","."]
+		,["6",".",".","1","9","5",".",".","."]
+		,[".","9","8",".",".",".",".","6","."]
+		,["8",".",".",".","6",".",".",".","3"]
+		,["4",".",".","8",".","3",".",".","1"]
+		,["7",".",".",".","2",".",".",".","6"]
+		,[".","6",".",".",".",".","2","8","."]
+		,[".",".",".","4","1","9",".",".","5"]
+		,[".",".",".",".","8",".",".","7","9"]
+	]))
+}
+ 
+main();
--- 3_b2bswe/__pseudo/02_arrays/4e_validSudoku.js
+++ 3_b2bswe/__pseudo/02_arrays/4e_validSudoku.js
@@ -10,10 +10,10 @@
 		rowMap{}, colMap{}, boxMap{}
 		(j < 9):
 			row = m.i.j; col = m.j.i
-			box = m[3(i/3) + 3(j/3)]
-				   [3(i%3) + 3(j%3)]
+			box = m [3(i/3) + 3(j/3)]
+				    [3(i%3) + 3(j%3)]
 
-			if (row|col|box === '.')
+			if (row|col|box !== '.')
 				if (map[row|col|box]) return false
 				map[row|col|box] = 1
 
