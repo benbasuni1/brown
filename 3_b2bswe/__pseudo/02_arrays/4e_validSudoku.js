da70f75e39f48ef77113ac8f1cc5b14a68c5936b
Ben Basuni <benbasuni1@gmail.com>
2020-11-25 22:22:36 -0800

    spacing

--- 3_b2bswe/__pseudo/02_arrays/4e_validSudoku.js
+++ 3_b2bswe/__pseudo/02_arrays/4e_validSudoku.js
@@ -10,7 +10,7 @@
 		rowMap{}, colMap{}, boxMap{}
 		(j < 9):
 			row = m.i.j; col = m.j.i
-			box = m [3(i/3) + 3(j/3)]
+			box = m[3(i/3) + 3(j/3)]
 				    [3(i%3) + 3(j%3)]
 
 			if (row|col|box !== '.')
