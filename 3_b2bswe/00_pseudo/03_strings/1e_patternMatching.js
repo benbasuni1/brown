fd59138c73dbc348f614a785acb1547dbe169601
Ben Basuni <benbasuni1@gmail.com>
2020-11-25 22:41:37 -0800

    patternmatch

--- 3_b2bswe/00_pseudo/03_strings/1e_patternMatching.js
+++ 3_b2bswe/00_pseudo/03_strings/1e_patternMatching.js
@@ -3,8 +3,9 @@
 	1. Pattern Matching
 	Input  : (<Array <str words>, <String pattern>) 
 	Output : (<Bool>)
-	*/
-{
+*/
+
+patternMatch(<Array <String words>>, <String pattern>) : <Bool> {
 	result = []
 
 	(word of words):
@@ -18,9 +19,10 @@ patternCheck(<String s1>, <String s2>) : <Bool> {
 	s1Map, s2Map {}
 	(i < len(s2)):
 		if (!s1Map[s1.i]) s1Map[s1.i] = s2Char;
-		if (!s2Map[s2.i]) s1Map[s2.i] = s1Char;
+		if (!s2Map[s2.i]) s2Map[s2.i] = s1Char;
 
-		if (s1Map[s1.i] !== s2Char || s2Map[s2.i] !== s1Char) 
+		if (s1Map[s1.i] !== s2Char || 
+			s2Map[s2.i] !== s1Char) 
 			return false
 	
 	return true
